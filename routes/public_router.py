05c92fb4edc029147ec41ff41b46a12473f4eca7
JulyLemos <julylemos1@hotmail.com>
2024-11-28 16:07:46 -0300

    Commit 16 - Biblioteca

--- routes/public_router.py
+++ routes/public_router.py
@@ -29,7 +29,12 @@ def get_lar(request: Request):
 
 #rota para página de cadastro - com sql e mudando de página para alteração
 @router.get("/cadastro", name="cadastro")
-def get_login(request: Request): #mudar depois
+def get_login(request: Request):
+    response = templates.TemplateResponse("estante.html", {"request": request})
+    return response
+
+@router.get("/estante", name="cestante")
+def get_login(request: Request): 
     response = templates.TemplateResponse("cadastro.html", {"request": request})
     return response
 
